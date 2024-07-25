b9cbe2a82d406f62906e3c261c38ea615fe85ae4
sandratraniaina <sandratrarafa@gmail.com>
2024-07-25 21:37:09 +0300

    feat: Add content to the application to configure layout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,11 @@ import './App.css';
 const App = () => {
     return (
         <div className='wrapper'>
-            <header className='header'></header>
+            <header className='header'>Header</header>
             <main className='main'>
-                <section className='task-column'></section>
-                <section className='task-column'></section>
-                <section className='task-column'></section>
+                <section className='task-column'>Task section 1</section>
+                <section className='task-column'>Task section 2</section>
+                <section className='task-column'>Task section 3</section>
             </main>
         </div>
     )
