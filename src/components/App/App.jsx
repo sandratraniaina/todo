caf85c2e3a7b9ccffe9695c87f5ffdef4f4e7b18
sandratraniaina <sandratrarafa@gmail.com>
2024-08-02 17:17:34 +0300

    refactor: Refactor project structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
 import './App.css';
-import TaskForm from '../TaskForm/TaskForm';
 
 const App = () => {
     return (
-        <div className='wrapper'>
-            <header className='header'>
-                <TaskForm />
-            </header>
-            <main className='main'>
-                <section className='task-column'>Task section 1</section>
-                <section className='task-column'>Task section 2</section>
-                <section className='task-column'>Task section 3</section>
-            </main>
-        </div>
     )
 };
 
